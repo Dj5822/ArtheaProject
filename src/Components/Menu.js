5dccfa32b957c1d308c150525eb532119197d818
dj5822 <dj5822@gmail.com>
2020-12-28 20:26:03 +1300

    Added basic styling to menu

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import './Menu.scss';
 
 class Menu extends React.Component {
 
   render() {
     return (
-      <div>
+      <div id="Menu">
         <h1>Arthea Project</h1>
         <Link to="/short-stories">
           <button>Short Stories</button>
--- src/Components/ShortStories.js
+++ src/Components/ShortStories.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+
+class NavBar extends React.Component {
+  render() {
+    return (
+      <div>
+        <Link to="/">
+          <button>Return to Menu</button>
+        </Link>
+        <button>Story 1</button>
+      </div>
+    );
+  }
+}
+
+class SideBar extends React.Component {
+  render() {
+    return (
+      <div>
+        <button>Sub Story 1</button>
+      </div>
+    );
+  }
+}
 
 class ShortStories extends React.Component {
   render() {
     return (
       <div>
-        <p>This is short stories</p>
+        <NavBar />
+        <SideBar />
       </div>
     );
   }
