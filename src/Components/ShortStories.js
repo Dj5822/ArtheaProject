3c9bf8c17f0f78b9d24872e30a7cbe90d80c42b0
dj5822 <dj5822@gmail.com>
2020-12-28 23:09:12 +1300

    Added button highlight on select

--- src/Components/ShortStories.js
+++ src/Components/ShortStories.js
@@ -6,34 +6,72 @@ import './ShortStories.scss';
 class NavBar extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      selectedNavButton: 0
+    }
+
     this.navButtonPressed = this.navButtonPressed.bind(this);
   }
 
-  navButtonPressed(event) {
-
+  navButtonPressed(num) {
+    this.setState({
+      selectedNavButton: num
+    });
   }
 
   render() {
+    let buttonClassList = new Array(2).fill("navButton");
+
+    for (var i=0; i<buttonClassList.length; i++){
+      if (this.state.selectedNavButton === i+1) {
+        buttonClassList[i] = "selectedNavButton";
+      }
+    }
 
     return (
       <div id="navbar">
         <Link to="/">
           <button id="returnButton" className="navButton">&lt;</button>
         </Link>
-        <button id="button1" className="navButton" onClick={this.navButtonPressed}>Story 1</button>
-        <button id="button2" className="navButton" onClick={this.navButtonPressed}>Story 2</button>
+        <button id="button1" className={buttonClassList[0]} onClick={() => this.navButtonPressed(1)}>Story 1</button>
+        <button id="button2" className={buttonClassList[1]} onClick={() => this.navButtonPressed(2)}>Story 2</button>
       </div>
     );
   }
 }
 
 class SideBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedSideButton: 0
+    }
+
+    this.sideButtonPressed = this.sideButtonPressed.bind(this);
+  }
+
+  sideButtonPressed(num) {
+    this.setState({
+      selectedSideButton: num
+    });
+  }
+
   render() {
+    let buttonClassList = new Array(3).fill("sideButton");
+
+    for (var i=0; i<buttonClassList.length; i++){
+      if (this.state.selectedSideButton === i+1) {
+        buttonClassList[i] = "selectedSideButton";
+      }
+    }
+
     return (
       <div id="sidebar">
-        <button className="sideButton">Chapter 1</button>
-        <button className="sideButton">Chapter 2</button>
-        <button className="sideButton">Chapter 3</button>
+        <button className={buttonClassList[0]} onClick={() => this.sideButtonPressed(1)}>Chapter 1</button>
+        <button className={buttonClassList[1]} onClick={() => this.sideButtonPressed(2)}>Chapter 2</button>
+        <button className={buttonClassList[2]} onClick={() => this.sideButtonPressed(3)}>Chapter 3</button>
       </div>
     );
   }
