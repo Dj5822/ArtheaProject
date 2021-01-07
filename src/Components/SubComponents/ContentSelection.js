3f5a3ec32790349441295293d6cdd1476d5e0b3e
dj5822 <dj5822@gmail.com>
2021-01-07 16:13:19 +1300

    Added ContentSelection

--- src/Components/SubComponents/ContentSelection.js
+++ src/Components/SubComponents/ContentSelection.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import './ContentSelection.scss'
 
 class ContentSelection extends React.Component {
   render() {
-    let currentText = "";
+    let selectedStory = {};
+    let buttonsList = [];
     if (this.props.currentSub !== ""){
-      currentText = this.props.data[this.props.currentMain][this.props.currentSub];
+      selectedStory = this.props.data[this.props.currentMain][this.props.currentSub];
+    }
+
+    for (var i=0; i<Object.keys(selectedStory).length; i++) {
+      buttonsList.push(
+        <button class="selectionButton">
+          {Object.keys(selectedStory)[i]}
+        </button>);
     }
 
     return (
       <div id="content">
-        <button>{currentText}</button>
-        <button>{currentText}</button>
-        <button>{currentText}</button>
+        {buttonsList}
       </div>
     );
   }
